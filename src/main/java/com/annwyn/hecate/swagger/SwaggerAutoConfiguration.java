8f15d6129c14ccd2a36c0bef6f44919cf4878e5b
赵家禄 <zhaojialu@yto.net.cn>
2020-11-18 11:02:40 +0800

    更新swagger为3.0.0

--- src/main/java/com/annwyn/hecate/swagger/SwaggerAutoConfiguration.java
+++ src/main/java/com/annwyn/hecate/swagger/SwaggerAutoConfiguration.java
@@ -8,27 +8,18 @@ import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguratio
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 @EnableConfigurationProperties(SwaggerProperties.class)
 @ConditionalOnProperty(prefix = "com.annwyn.swagger", name = "enable", havingValue = "true")
 @AutoConfigureAfter(WebMvcAutoConfiguration.class)
@@ -48,45 +39,14 @@ public class SwaggerAutoConfiguration {
                 .version(this.swaggerProperties.getVersion()) //
                 .description(this.swaggerProperties.getDescription()) //
                 .build();
-        return new Docket(DocumentationType.SWAGGER_2) //
-                .enable(this.swaggerProperties.isEnable())
+        return new Docket(DocumentationType.OAS_30) //
+                .pathMapping(this.swaggerProperties.getServletPath()) //
+                .enable(this.swaggerProperties.isEnable()) //
                 .apiInfo(apiInfo) //
                 .select() //
                 .apis(RequestHandlerSelectors.basePackage(this.swaggerProperties.getBasePackage())) //
                 .paths(PathSelectors.any()) //
-                .build()
-                .globalOperationParameters(this.buildParameters());
+                .build();
     }
 
-    private List<Parameter> buildParameters() {
-        if(CollectionUtils.isEmpty(this.swaggerProperties.getParameters())) {
-            return Collections.emptyList();
-        }
-
-        List<Parameter> parameters = new ArrayList<>(this.swaggerProperties.getParameters().size());
-        for(SwaggerProperties.SwaggerParameter template : this.swaggerProperties.getParameters()) {
-            ParameterBuilder parameterBuilder = new ParameterBuilder() //
-                    .name(template.getName())
-                    .description(template.getDescription())
-                    .modelRef(new ModelRef(template.getModelReference()))
-                    .parameterType(template.getParameterType())
-                    .required(template.isRequired());
-            parameters.add(parameterBuilder.build());
-        }
-        return parameters;
-    }
-
-    /**
-     * 此处不配置也依旧可以正常访问, 应该是swagger内部有做映射, 不过暂时不清楚是哪处做的配置.
-     */
-    @Bean
-    public WebMvcConfigurer swaggerWebMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-                registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-            }
-        };
-    }
 }
--- src/main/java/com/annwyn/hecate/swagger/SwaggerProperties.java
+++ src/main/java/com/annwyn/hecate/swagger/SwaggerProperties.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.List;
-
 
 @Getter
 @Setter
@@ -21,54 +19,26 @@ public class SwaggerProperties {
      * swagger title
      */
     private String title;
+
     /**
      * swagger version
      */
     private String version;
+
     /**
      * swagger description
      */
     private String description;
 
     /**
+     * 请求地址
+     */
+    private String servletPath;
+
+    /**
      * swagger扫描包
      */
     private String basePackage;
 
-    /**
-     * 共同请求参数
-     */
-    private List<SwaggerParameter> parameters;
-
-    @Getter
-    @Setter
-    public static final class SwaggerParameter {
-
-        /**
-         * 参数名称
-         */
-        private String name;
-
-        /**
-         * 参数说明
-         */
-        private String description;
-
-        /**
-         * 是否必须项
-         */
-        private boolean required;
-
-        /**
-         * 参数位置, header, cookie, body, query
-         */
-        private String parameterType;
-
-        /**
-         * 参数类型
-         */
-        private String modelReference;
-
-    }
 
 }
